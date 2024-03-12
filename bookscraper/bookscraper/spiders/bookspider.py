cc575d688dcb39b1e940c66849b5785c85081c71
Sicelo Mduna <122819041+Sicelo-Mzingisi-Mduna@users.noreply.github.com>
2024-03-12 23:55:44 +0200

    How to crawl relative-url's with scrapy spider
                                 Part 1
    
    1) We want the scrapy spider to click and follow(crawl) a page that has a relative_url(hyperlink that open up a new  page)
    
    2) I also added a word document under documentation for the different Portfolio projects i want to do using the knowledge I learn in this tutorial and future tutorials called (Project Ideas)

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -9,21 +9,20 @@ class BookspiderSpider(scrapy.Spider):
     def parse(self, response):
         Books = response.css('article.product_pod')
         for book in Books:
-            yield{
-                
-                'title' : book.css('h3 a::attr(title)').get(),
-                'price' : book.css('.price_color::text').get(),
-                'url' : book.css('h3 a::attr(href)').get()  
-            }
-        next_page = response.css('li.next a::attr(href)').get()
-        if next_page is not None:
-            if 'catalogue/' in next_page:
+            relative_url = book.css(' h3 a ::attr(href)').get()
+            if 'catalogue/' in relative_url:
                 string_1 = 'https://books.toscrape.com/'
-                string_2 = next_page
-                next_page = string_1 + string_2
+                string_2 = relative_url
+                book_url = string_1 + string_2
             else:
                 string_1 = 'https://books.toscrape.com/catalogue/'
-                string_2 = next_page
-                next_page = string_1 + string_2
-            yield response.follow(next_page, callback=self.parse)
+                string_2 = relative_url
+                book_url = string_1 + string_2
+            yield response.follow(book_url, callback=self.parse_book_page)
+     
+    def parse_book_page(self, response):
+       pass      
+            
+    
         
+ 
