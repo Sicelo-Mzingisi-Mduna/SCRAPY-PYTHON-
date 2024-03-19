edc0630dfccff15596cdebe99d1d42f9f35d0123
Sicelo Mduna <122819041+Sicelo-Mzingisi-Mduna@users.noreply.github.com>
2024-03-19 19:57:52 +0200

    Updated Scrapy spider
    1) Updated the documentation
    
    2) Updated the scrapy spider so that it goes into each individual book and it retrieves information, this is done for all the 1051 pages on the website
    
    Date: 2024 / 03 / 19 ( 20: 00)

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -19,9 +19,36 @@ class BookspiderSpider(scrapy.Spider):
                 string_2 = relative_url
                 book_url = string_1 + string_2
             yield response.follow(book_url, callback=self.parse_book_page)
+        
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            if 'catalogue/' in next_page:
+                string_1 = 'https://books.toscrape.com/'
+                string_2 = next_page
+                next_page_url = string_1 + string_2
+            else:
+                string_1 = 'https://books.toscrape.com/catalogue/'
+                string_2 = next_page
+                next_page_url = string_1 + string_2
+            yield response.follow(next_page_url, callback=self.parse)
      
     def parse_book_page(self, response):
-       pass      
+        yield{
+            " Book_title " : response.css('#content_inner > article > div.row > div.col-sm-6.product_main > h1::text').get(),
+        
+            " Book_category " : response.css('#default > div > div > ul > li:nth-child(3) > a::text').get().strip(),
+        
+            " Book_description " : response.css('#content_inner > article > p::text').get(),
+            
+            "Product type" : response.css('tr:nth-child(2) > td::text').get(),
+            
+            "Price (incl. tax)" : response.css('tr:nth-child(4) > td::text').get(),
+            
+            " Availability " : response.css('tr:nth-child(6) > td::text').get()
+        }
+        
+        
+             
             
     
         
