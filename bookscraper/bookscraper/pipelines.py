f9495329f15cf630cd2cf9048b63efc1fb7b74d1
Sicelo Mduna <122819041+Sicelo-Mzingisi-Mduna@users.noreply.github.com>
2024-03-23 12:46:01 +0200

    Created a pipeline
    Created a pipeline which is used to clean data that has been scraped
    
    *Basically items are used in pipelines to clean an csv output file

--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -10,4 +10,31 @@ from itemadapter import ItemAdapter
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
-        return item
+        
+        adapter = ItemAdapter(item)
+        
+        ##Strip all whitespaces from strings
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != 'description':
+                value = adapter.get(field_name)
+                adapter[field_name]= value[0].strip()
+                
+        ## Category & Product Type --> switch to lowercase
+        lowercase_keys = ['category', 'product_type'] #specify specific keys\items
+        for lowercase_key in lowercase_keys:
+            value = adapter.get(lowercase_key)
+            adapter[lowercase_key] = value.lower()
+            
+        ## Price --> convert to float
+        price_keys = ['price']
+        for price_key in price_keys:
+            value = adapter.get(price_key)
+            value = value.replace('£', '') #replace the pound sign with the rand sign
+            adapter[price_key] = float(value)
+            adapter[price_key] = adapter[price_key] * 24.0
+            adapter[price_key] = str(adapter[price_key])
+            adapter[price_key] = "R" + adapter[price_key]
+            
+        
+        return item 
