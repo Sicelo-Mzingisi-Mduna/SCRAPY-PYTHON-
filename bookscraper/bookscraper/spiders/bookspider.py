54d0bc647d82e0f56118a56f419f5f14af13d026
Sicelo Mduna <122819041+Sicelo-Mzingisi-Mduna@users.noreply.github.com>
2024-04-04 20:21:13 +0200

    Implimented changes to extract data to a database

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -1,54 +1,58 @@
 import scrapy
-from bookscraper.items import BookItem
-
+from bookscraper.items import BookItem  
 
 class BookspiderSpider(scrapy.Spider):
     name = 'bookspider'
     allowed_domains = ['books.toscrape.com']
-    start_urls = ['https://books.toscrape.com']
+    start_urls = ['https://books.toscrape.com/']
+
+    custom_settings = {
+        'FEEDS': {
+            'booksdata.json': {'format': 'json', 'overwrite': True},
+        }
+    }
 
     def parse(self, response):
-        Books = response.css('article.product_pod')
-        for book in Books:
-            relative_url = book.css(' h3 a ::attr(href)').get()
+        books = response.css('article.product_pod')
+        for book in books:
+            relative_url = book.css('h3 a ::attr(href)').get()
+
             if 'catalogue/' in relative_url:
-                string_1 = 'https://books.toscrape.com/'
-                string_2 = relative_url
-                book_url = string_1 + string_2
+                book_url = 'https://books.toscrape.com/' + relative_url
             else:
-                string_1 = 'https://books.toscrape.com/catalogue/'
-                string_2 = relative_url
-                book_url = string_1 + string_2
+                book_url = 'https://books.toscrape.com/catalogue/' + relative_url
             yield response.follow(book_url, callback=self.parse_book_page)
-        
-        next_page = response.css('li.next a::attr(href)').get()
+
+        next_page = response.css('li.next a ::attr(href)').get()
         if next_page is not None:
             if 'catalogue/' in next_page:
-                string_1 = 'https://books.toscrape.com/'
-                string_2 = next_page
-                next_page_url = string_1 + string_2
+                next_page_url = 'https://books.toscrape.com/' + next_page
             else:
-                string_1 = 'https://books.toscrape.com/catalogue/'
-                string_2 = next_page
-                next_page_url = string_1 + string_2
+                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
             yield response.follow(next_page_url, callback=self.parse)
-     
+
+
     def parse_book_page(self, response):
-        book_items = BookItem()
-        
-        book_items['title'] = response.css('#content_inner > article > div.row > div.col-sm-6.product_main > h1::text').get(),
-        
-        book_items['category'] = response.css('#default > div > div > ul > li:nth-child(3) > a::text').get().strip(),
-        
-        book_items['description'] = response.css('#content_inner > article > p::text').get(),
-            
-        book_items['product_type'] = response.css('tr:nth-child(2) > td::text').get(),
-            
-        book_items['price'] = response.css('tr:nth-child(4) > td::text').get(),
-            
-        book_items['availability'] = response.css('tr:nth-child(6) > td::text').get()
-        
-        yield book_items
+
+        table_rows = response.css("table tr")
+        book_item = BookItem()
+
+    
+        book_item['url'] = response.url,
+        book_item['title'] = response.css('.product_main h1::text').get(),
+        book_item['upc'] = table_rows[0].css("td ::text").get()
+        book_item['product_type' ] = table_rows[1].css("td ::text").get(),
+        book_item['price_excl_tax'] = table_rows[2].css("td ::text").get(),
+        book_item['price_incl_tax'] = table_rows[3].css("td ::text").get(),
+        book_item['tax'] = table_rows[4].css("td ::text").get(),
+        book_item['availability'] = table_rows[5].css("td ::text").get(),
+        book_item['num_reviews']=  table_rows[6].css("td ::text").get(),
+        book_item['stars'] = response.css("p.star-rating").attrib['class'],
+        book_item['category'] = response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
+        book_item['description'] = response.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
+        book_item['price'] = response.css('p.price_color ::text').get(),
+    
+        yield book_item
         
         
              
